cfbee27f62e4c3c488035510e2d661ec6ec8d0fc
Roophee <alex.babaew@gmail.com>
2020-12-29 11:01:17 +0200

    add oop version of world

--- index.js
+++ index.js
@@ -1,61 +1,85 @@
-const dog = {
-    species: 'dog',
-    name: 'Milo',
-    gender: 'male',
-    legs: 4,
-    hands: 0,
-    tail: 1,
-    saying: 'Woof-Woof!',
-    friends: ['Sam', 'Hanna', 'Kitty']
-},
-    cat = {
-    species: 'cat',
-    name: 'Kitty',
-    gender: 'female',
-    legs: 4,
-    hands: 0,
-    tail: 1,
-    saying: 'Meow!',
-    friends: []
-},
-    man = {
-    species: 'human',
-    name: 'Sam',
-    gender: 'male',
-    legs: 2,
-    hands: 2,
-    tail: 0,
-    saying: 'Hello, my friend!',
-    friends: ['Hanna',]
-},
-    woman = {
-    species: 'human',
-    name: 'Hanna',
-    gender: 'female',
-    legs: 2,
-    hands: 2,
-    tail: 0,
-    saying: 'Hi, I\`m Hanna!',
-    friends: ['Sam', 'Kitty', 'Milo']
-},
-    womanCat = {
-    species: 'human',
-    name: 'Sara',
-    gender: 'female',
-    legs: 2,
-    hands: 2,
-    tail: 1,
-    saying: cat.saying,
-    friends: ['Sam', 'Hanna', 'Kitty', 'Milo']
-};
+"use strict";
 
-const population = [man, woman, cat, dog, womanCat],
-    paramsOfItem = ['species', 'name', 'saying', 'gender', 'legs', 'hands', 'tail','friends'];
+class Animalia {
+    alive = true;
+    canSaying = false;
+    constructor(gender, legs, tail) {
+        this.legs = legs;
+        this.tail = tail;
+        this.gender = gender;
+    }
+}
 
-let makeOutputString = item => {
-    return paramsOfItem.map(param => item[param]).join(';');
-};
+class Mammalia extends Animalia {
+    canSaying = true;
+    gregarious = true;
+    description (arrayOfProperties) {
+        return arrayOfProperties.map(property =>this[property]).join(';')
+    }
+    constructor(gender, friends, legs, tail) {
+        super(gender, legs, tail);
+        this.friends = friends.split(',');
 
-population.forEach(anyAnimal => {
-    print(makeOutputString(anyAnimal));
-});
+    }
+}
+
+class HomoSapiens extends Mammalia {
+    species = "human";
+    hasName = true;
+    constructor(name, gender, friends, sayingText) {
+        super(gender, friends);
+        this.name = name;
+        this.legs = 2;
+        this.hands = 2;
+        this.tail = 'none';
+        this.saying = sayingText;
+    }
+}
+
+class Dog extends Mammalia {
+    species = "dog";
+    hasName = true;
+    static saying = "Woof-Woof!";
+    constructor(name, gender, friends) {
+        super(gender, friends);
+        this.legs = 4;
+        this.hands = 'none';
+        this.tail = 1;
+        this.name = name;
+        this.saying = Dog.saying;
+    }
+}
+
+class Cat extends Mammalia {
+    species = "cat";
+    hasName = true;
+    static saying = "Meow!";
+    constructor(name, gender, friends) {
+        super(gender, friends);
+        this.legs = 4;
+        this.hands = 'none';
+        this.tail = 1;
+        this.name = name;
+        this.saying = Cat.saying;
+    }
+}
+
+class CatWoman extends HomoSapiens {
+    species = "cat-woman";
+    catSaying() {
+        return Cat.saying;
+    }
+    constructor(name, gender, friends) {
+        super(name, gender, friends);
+        this.saying = this.catSaying();
+    }
+}
+const properties = ['species', 'name', 'gender', 'legs', 'hands', 'tail', 'saying', 'friends'],
+    man = new HomoSapiens('Sam', 'male', 'Hanna, Milo', 'Honey, I\'m home'),
+    woman = new HomoSapiens('Hanna', 'female', 'Sam, Milo, Kitty', "I\'m traveling down the river."),
+    dog = new Dog('Milo', 'male', 'Sam, Hanna'),
+    cat = new Cat("Kitty", 'female', ''),
+    catWoman = new CatWoman('Susanna', 'female', 'Sam, Hanna, Milo, Kitty'),
+    tinyWorld = [man, woman, dog, cat, catWoman];
+
+print(tinyWorld.map(item => item.description(properties)).join('\n'))
